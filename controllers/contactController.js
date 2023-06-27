fccf229851d141988eba47a5d90cd7726fe3cd80
Mohit0190 <mohitgagneja934@gmail.com>
2023-06-27 18:57:26 +0530

    add routes and controllers

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,17 +3,20 @@ const create = (req,res)=>{
   res.status(201).json({message:'post'})
 }
 
-const get = (req,res)=>{
-    res.status(200).json({message:'post'})
+const geT = (req,res)=>{
+    res.status(200).json({message:'get'})
+}
+const getwithId = (req,res)=>{
+    res.status(200).json({message:'get with id'})
 }
 
 const replace = (req,res)=>{
-    res.status(200).json({message:'post'})
+    res.status(200).json({message:'replace'})
 }
 const update = (req,res)=>{
-    res.json({message:'post'})
+    res.json({message:'update'})
 }
 const del = (req,res)=>{
-    res.json({message:'post'})
+    res.json({message:'del'})
 }
-module.exports = {create,get,replace,update,del};
+module.exports = {create,geT,replace,update,del,getwithId};
